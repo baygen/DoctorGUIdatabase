ea0c571a3dd0e6f658ce29108c6fbf7457a4fadf
Buy <Buy@192.168.0.106>
2017-04-22 17:51:53 +0300

    Creatted Interface for develop manage functionality

---
+++ src/com/fordoctor/manage/MainFunctionsInterface.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fordoctor.manage;
+
+import com.forDoctors.entity.Seanse;
+
+/**
+ *
+ * @author Buy
+ */
+public interface MainFunctionsInterface {
+    
+    public Seanse getSeanseFromGUI();    
+    
+    
+}
