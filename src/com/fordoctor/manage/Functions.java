f5ceed4c5395d21851dbe7bf589525f4da05978a
Buy <Buy@192.168.0.106>
2017-04-24 16:24:27 +0300

    Creatted functionality

---
+++ src/com/fordoctor/manage/Functions.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fordoctor.manage;
+
+import com.main_frame.api.MainFrame;
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Buy
+ */
+public class Functions {
+    
+    public String[] getArrayForComboDate(){
+        LocalDate firstDate =MainFrame.getSeanseDay();
+        LocalDate addedDay;
+        String[] dates = new String[12];
+            for(int i=0;i<10;i++){
+                addedDay=firstDate;
+                dates[i]=addedDay.toString();
+                
+            }
+        return dates;
+    }
+}
